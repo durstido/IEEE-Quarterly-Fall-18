0b50adabcf4e6bed05398ea241d410df877830e2
durstido <durstido@gmail.com>
2018-11-26 22:52:32 -0800

    Update frontend.py
    full_list will return an array, where each value is a string of the form: "modifiedDate-distancefromcenter-privacy-product-address" (distancefromcenter is now a placeholder; privacy is 1 for yes and 0 for no)

--- frontend.py
+++ frontend.py
@@ -1,32 +1,53 @@
 from flask import Flask, request
+import time
 
 app = Flask(__name__)
 
+full_list = []
 products = []
 addresses = []
 delays = []
 privacies = []
+dates = []
+full_dates=[]
+i = 0
 
 @app.route('/', methods=['GET', 'POST'])
-def form_example():
-    #if request.method == 'POST':
+def frontend():
+        global i
         if request.form.get('submit') == 'Submit':
             product = request.form.get('product').encode("utf-8")
             address = request.form.get('address').encode("utf-8")
             delay=request.form['option1'].encode("utf-8")
             privacy=request.form['option2'].encode("utf-8")
 
+            date_year = int(float(time.strftime('%Y', time.localtime(time.time()))))
+            date_month = int(float(time.strftime('%m', time.localtime(time.time()))))
+            date_day = int(float(time.strftime('%d', time.localtime(time.time()))))
+                
             products.append(product)
             addresses.append(address)
             delays.append(delay)
             privacies.append(privacy)
+            dates.append([date_year, date_month, date_day])
 
-        print(products)
-        print(delays)
-        print(privacies)
+        if i>0 and request.form["submit"] == "Submit":
+                if delays[i-1] == "yes, for one day":
+                        dates[i-1][2] += 1
+                elif delays[i-1] == "yes, for two days":
+                        dates[i-1][2] += 2
+                if privacies[i-1] == "yes, please!":
+                        privacies[i-1] = 1
+                elif privacies[i-1] == "no, I prefer my privacy":
+                        privacies[i-1] = 0
+                
+                full_dates.append(str(dates[i-1][0]) + ":" + str(dates[i-1][1]) + ":" + str(dates[i-1][2]))
+                full_list.append(full_dates[i-1] + "-" + "PLACEHOLDER" + "-" + str(privacies[i-1]) + "-" + products[i-1] + "-" + addresses[i-1])
+        
+        i += 1
         
         if request.form.get('submit') == 'Stop':
-            return "thank you for shopping with us"
+            return "Thank you for shopping with us!" 
 
         return '''<form method="POST">
                   Product: <input type="text" name="product"><br>
@@ -41,6 +62,9 @@ def form_example():
                   <input type="submit" name="submit" value="Submit">
                   <input type="submit" name="submit" value="Stop"><br>
               </form>'''
-    
+
+
+        return full_list
+
 if __name__ == '__main__':
     app.run(host="localhost")
