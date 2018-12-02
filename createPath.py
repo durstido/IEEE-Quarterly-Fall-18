3b75df68e617a519cc30eaf220ecbd77b8c09559
durstido <durstido@gmail.com>
2018-12-01 22:47:46 -0800

    Update createPath.py

--- createPath.py
+++ createPath.py
@@ -1,18 +1,22 @@
 from geoloc import addressToDistance
-import numpy as np
 import googlemaps
-import folium
-import math
 
 origin = "9500 Gilman Drive, La Jolla, CA"
 
-def Path(addresses):
+def Path(addresses): #given a list of string addresses
     gmaps = googlemaps.Client(key='AIzaSyAZ5896c7UzrpG99I_PCdlFBebE0jpAlVM')
 
     distances = []
     dis = {}
     distances1 = {}
 
+    #create a dictionary where each key is an address, and each value is another dictionary
+    #each nested dictionary will have all the other addresses (not the one with the original key), and the values will be their distance to the original key/address
+    #for example, for nodes = ('A', 'B', 'C'), the dictionary distances1 will be
+    #{ 'A': {'B': 3, 'C': 4}
+    #  'B': {'A': 3, 'C': 8}
+    #  'C': {'A': 4, 'B': 8}
+    
     for entry in addresses:
         distances.append(entry)
         for i in range(len(addresses)):
@@ -21,9 +25,13 @@ def Path(addresses):
                 distances1[entry] = dis
 
     add=[]
-    print(distances1)
 
     #all-nodes shortest path algorithm taken (and modified) from https://stackoverflow.com/questions/38442830/all-nodes-shortest-paths
+    #essentially, it will start with the inital value/base (in our example, UCSD), and will check through distances1 the distances to all other points
+        #it will then choose the point with the least distance and move to it
+        #it will repeat this process with this point, finding the next nearest point (excluding the previous points of course), and so on
+        #or at least, this is what we understood of it :)
+    
     for start in addresses:
         nodes = addresses
         distances = distances1
@@ -45,44 +53,10 @@ def Path(addresses):
             candidates = [node for node in unvisited.items() if node[1]]
             current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
 
-        #our continued code
-        if start == origin:
-            for i in visited.keys():
+        #our own code from here on
+        if start == origin: #start at base/origin (UCSD)
+            for i in visited.keys(): #all addresses in the order of shortest path 
                     loc = gmaps.geocode(i)
-                    x = ((loc[0]['geometry']['location']['lat']), (loc[0]['geometry']['location']['lng']))
+                    x = ((loc[0]['geometry']['location']['lat']), (loc[0]['geometry']['location']['lng'])) #lat and long of each point
                     add.append(x)
-    
     return add
-
-#example:
-'''
-address4 = "4296 Corte Langostino, San Diego, CA"
-address3 = "12871 Pine Meadow Ct, San Diego, CA"
-address2 = "4472 Heritage Glen Ln, San Diego, CA"
-
-address5 = "8175 Regents Rd, San Diego, CA"
-address6 = "5544 Regents Rd, San Diego, CA"
-origin = "9500 Gilman Drive, La Jolla, CA"
-address7 = "2966 Massasoit Ave, San Diego, CA"
-gmaps = googlemaps.Client(key='AIzaSyAZ5896c7UzrpG99I_PCdlFBebE0jpAlVM')
-
-addresses = [origin, address2, address3, address4]
-points = Path(addresses)
-
-lat = []
-Long = []
-
-for i in range(len(addresses)):
-    loc = gmaps.geocode(addresses[i])
-    lat.append(loc[0]['geometry']['location']['lat'])
-    Long.append(loc[0]['geometry']['location']['lng'])
-
-mapit = folium.Map(location=[lat[0], Long[0]], zoom_start=12)
-for i in range(len(lat)):
-    folium.Marker(location=[lat[i], Long[i]]).add_to( mapit )
-
-folium.PolyLine(points).add_to(mapit)
-
-mapit.save('/Users/durstido/Desktop/map.html')
-'''
-
