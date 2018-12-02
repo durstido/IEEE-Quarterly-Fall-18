86b6568022a208da419d4d9cd7d371175e238bd4
bobbytatum27 <bobbyt9927@yahoo.com>
2018-12-01 21:04:16 -0800

    Update algo.py

--- algo.py
+++ algo.py
@@ -29,20 +29,38 @@ def order(orderList, today):
         date = order[0]
         day = dateParse(date)
 
+        privacy = order[2]
+
 
         #'order' all of today's orders
         if (day == today):
-            print('ordering ' + today + ': \n\t' + ', '.join(orderList.pop(0)))
+            print('ordering day ' + today +
+                  ': \n\t' + ', '.join(orderList.pop(0)))
             print("\n\n")
+
+def lumpedOrders(orderList, today):
+    lumpedOrderList = []
+
+    listLength = len(orderList)
+    for i in range(0, listLength - 1):
+        if (orderList[i][4] == orderList[i+1][4] or
+            orderList[i][4] == orderList[i-1][4]):
+            lumpedOrderList.append(orderList[i])
+        if (i == listLength - 2):
+            if (orderList[i+1][4] == orderList[0][4]):
+                lumpedOrderList.append(orderList[i+1])
+
+    return lumpedOrderList
+
 
 
 
 user1 = '2018:11:20-1.23-0-product1-123 Test Street, City, CA'
-user2 = '2018:11:22-3.534-0-productXYz-456 Test Street, City, CA'
+user2 = '2018:11:22-3.534-0-product2-456 Test Street, City, CA'
 user3 = '2018:11:24-0.2342-0-product1-789 Test Street, City, CA'
-user4 = '2018:11:29-9.2342-1-product1-987 Test Street, City, CA'
-user5 = '2018:11:2-10.234212-1-product1-654 Test Street, City, CA'
-user6 = '2018:11:2-10.234212-1-product2-654 Test Street, City, CA'
+user4 = '2018:11:2-10.234212-0-product1-654 Test Street, City, CA'
+user5 = '2018:11:2-10.234212-1-product2-654 Test Street, City, CA'
+user6 = '2018:11:2-10.234212-1-product3-654 Test Street, City, CA'
 
 userList = [user1, user2, user3, user4, user5, user6]
 
@@ -51,11 +69,19 @@ for user in userList:
     addToOrderList(userInfo)
 
 
-print(privacyOrders)
-print(generalOrders)
+#print(privacyOrders)
+#print(generalOrders)
 
-#orders for the general orders
-for i in range(0,31):
-    day = str(i)
+lumpedOrderList = lumpedOrders(generalOrders, '2')
+addressList = []
+for order in lumpedOrderList:
+    addressList.append(order[4])
 
-    order(generalOrders, day)
+for order in generalOrders:
+    date = order[0]
+    day = dateParse(date)
+
+    if (day == '2'):
+        addressList.append(order[4])
+
+print (list(set(addressList)))
